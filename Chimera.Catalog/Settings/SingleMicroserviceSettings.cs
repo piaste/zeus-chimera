8c5772123c679cfd075778be335597bda33afc7e
Mauro Bussini <zenprogramming-it@gmail.com>
2019-09-05 12:32:27 +0200

    Added settings

---
+++ Chimera.Catalog/Settings/SingleMicroserviceSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chimera.Catalog.Settings
+{
+    public class SingleMicroserviceSettings
+    {
+        public string ProviderName { get; set; }
+
+        public string Url { get; set; }
+    }
+}
