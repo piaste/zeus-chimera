42f8f7ab2e080b02aaffd122f9f780fa52a909da
Stefano Pian <s.pian@zeus-lab.com>
2019-09-05 10:45:31 +0200

    cleanup

--- Chimera.Authentication.MongoDb/MongoDbRepository.fs
+++ Chimera.Authentication.MongoDb/MongoDbRepository.fs
@@ -15,7 +15,9 @@ open ZenProgramming.Chakra.Core.Data
 open MongoDB.Bson.IO
 
 
-
+/// <summary>
+/// Stores an entity inside a MongoDb database.
+/// <summary>
 type MongoDbRepository<'TEntity  
                         when 'TEntity : (new : unit -> 'TEntity)
                          and 'TEntity : not struct 
@@ -23,87 +25,56 @@ type MongoDbRepository<'TEntity
                          and 'TEntity : null
                     >
 
-                    /// Constructor
-                    ( dataSession : IMongoDbDataSession
-                    , repoValidation : 'TEntity -> ValidationResult seq
-                    , ?entityName : string
-                    // TODO: completare
-                    // , ?transaction : Transaction
+           /// <summary> Internal constructor </summary>
+           /// <param name="dataSession">A data session to an existing MongoDB database</param>
+           /// <param name="validationRules">Apply a function to the entity to verify any MongoDb-specific validation rules (eg. data size limits)</param>
+           /// <param name="collectionName">Customize the collection name. The default is the entity type's name.</param>
+           internal ( dataSession : IMongoDbDataSession                    
+                    , validationRules : 'TEntity -> ValidationResult seq
+                    , ?collectionName : string
                     ) = 
+        
     
 
-    // "mongodb:///zeus-mongo-01.japaneast.cloudapp.azure.com:27017"
-    // "chimera-catalog"
-    // "password"
-
-    let transaction : Transaction option = None
-    
     let db = dataSession.Database
 
-    let entityName = entityName |> Option.defaultValue (typeof<'TEntity>.Name)
+    /// If not specified, catalog name is the type name
+    let collectionName = collectionName |> Option.defaultValue (typeof<'TEntity>.Name)
     
-    let entities () = db.GetCollection(entityName)
+    let entities () = db.GetCollection(collectionName)
         
-
+    /// TODO : test the fake transaction implementation
+    let transaction : Transaction option = None
+        
+    /// Cast to IRepository
     member this.AsRepository = (this :> IRepository<'TEntity>)
 
     interface IRepository<'TEntity> with
         
         member this.Count(filterExpression) = 
-            let longCount : int64 = entities().CountDocuments(filterExpression)
+            let longCount = entities().CountDocuments(filterExpression)
             if longCount > (int64 Int32.MaxValue) then -1 else int longCount
 
         member this.Delete(entity) = 
-            let action() = 
-                entities().DeleteOne(fun (e : 'TEntity) -> entity.GetId() = e.GetId())
-                |> ignore
-            
-            match transaction with
-            | None -> action() 
-            | Some t -> t.Add(Delete entity, action)
-                
+            entities().DeleteOne(fun (e : 'TEntity) -> entity.GetId() = e.GetId())
+            |> ignore
 
         member this.Dispose() = 
             transaction |> Option.iter (fun t -> (t :> IDataTransaction).Rollback())
 
-
         member this.Fetch(filterExpression, startRowIndex, maximumRows, sortExpression, isDescending) = 
 
-
-            let filteredEntities = 
-                entities()
-                    .Find(filterExpression)
+            let filteredEntities = entities().Find(filterExpression)
                     
             let sort things = 
                 if isDescending then IFindFluentExtensions.SortByDescending<_, _>(things, sortExpression) 
                                 else IFindFluentExtensions.SortBy<_, _>(things, sortExpression)
 
             let sortedEntities = sort filteredEntities
+            
+            let boundedEntities = sortedEntities.Skip(startRowIndex).Limit(maximumRows)
 
-                        
-
-            let boundedEntities = 
-                sortedEntities
-                    .Skip(startRowIndex)
-                    .Limit(maximumRows)
-                    .ToList()
-
-            match transaction with
-            | None -> boundedEntities
-            | Some t -> 
-                let transactionUpdates = t.Fetch (filterExpression.Compile().Invoke)
-                
-                if Map.isEmpty transactionUpdates 
-                    then boundedEntities
-                    else ResizeArray [|
-                        for entity in boundedEntities do
-                            match Map.tryFind (getId entity) transactionUpdates with
-                            | Some (Existing entityUpdate)  -> yield entityUpdate
-                            | Some (Deleted _) -> ()
-                            | None -> yield entity
-                    |] // TODO: sort again
-
-            :> IList<_>
+            boundedEntities.ToList() :> IList<_>
             
 
         member this.GetSingle(expression) = 
@@ -115,18 +86,13 @@ type MongoDbRepository<'TEntity
             (this :> IRepository<_>).Validate(entity) |> Seq.isEmpty
 
         member this.Save(entity) = 
-            let action() = 
-                entities().InsertOne(entity)
-        
-            match transaction with
-            | None -> action() 
-            | Some t -> t.Add(Create entity, action)
+            entities().InsertOne(entity)
 
         member this.Validate(entity) = 
             let mutable results = ResizeArray<_>()
             let success = Validator.TryValidateObject(entity, ValidationContext entity, results)
 
-            results.AddRange(repoValidation(entity))
+            results.AddRange(validationRules(entity))
 
             results :> IList<_>
 
